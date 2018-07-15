bc96dd2e183fdc3332a98158c2a930c23e345e90
Michael Bestas <mkbestas@lineageos.org>
2018-07-15 22:54:36 +0300

    SystemUI: Add tunables for clock AM/PM style
    Change-Id: I7f27f135ae0d1e731c936792f655c115f107d20c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9316,6 +9316,17 @@ public final class Settings {
             "lockscreen_scramble_pin_layout";
 
         /**
+         * Display style of AM/PM next to clock in status bar
+         * 0: Normal display (Eclair stock)
+         * 1: Small display (Froyo stock)
+         * 2: No display (Gingerbread/ICS stock)
+         * default: 2
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+        /**
          * Whether or not to enable media resumption
          * When enabled, media controls in quick settings will populate on boot and persist if
          * resumable via a MediaBrowserService.
