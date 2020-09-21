67f3fbba6763fc4697acad3bbf369d70bf8d410b
Lucchetto <lucchetto.tie@live.com>
2020-09-21 14:07:20 +0200

    SystemUI: move brightness to bottom and show in quick qs
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I2fafe52a2a886fc460b07b250763190a15512cbd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1223,6 +1223,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     mQSBarHeader = ((QSFragment) qs).getHeader();
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+                    ((QuickStatusBarHeader)qs.getHeader()).getHeaderQsPanel().setBrightnessMirror(mBrightnessMirrorController);
                 }
             });
         }
